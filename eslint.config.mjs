5f43b177a977e6826439ead6bd4747d05399ad02
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-21 16:22:43 +0500

    chore(eslint): add sort-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   {
@@ -29,6 +30,38 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+
+  // simple-import-sort
+  {
+    name: 'app/simple-import-sort',
+    files: ['**/*.{ts,mts,tsx,vue,js,jsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Vue и внешние пакеты
+            ['^nestjs', '^@nestjs', '^@?\\w'],
+            // Внутренние пакеты
+            ['^@/modules(/.*|$)'],
+            ['^@/common(/.*|$)'],
+            // Импорты побочных эффектов
+            ['^\\u0000'],
+            // Родительские импорты
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Другие относительные импорты
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Стили
+            ['^.+\\.(css|scss|sass)$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
     },
   },
 );
