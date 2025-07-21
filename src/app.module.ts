527bc7fece682d3d975a8299b458f0be12522827
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-21 22:31:07 +0500

    feat(modules): add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { UsersModule } from '@/modules/users';
+
 import { envConfig } from '@/common/config/envConfig';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [envConfig] })],
+  imports: [ConfigModule.forRoot({ load: [envConfig] }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
