fb53ada06f194c5cd10d78d8ba2ef05af7f759c6
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-22 13:17:55 +0500

    feat(model): add wishlist entity

--- src/modules/wishlists/entities/wishlist.entity.ts
+++ src/modules/wishlists/entities/wishlist.entity.ts
@@ -1,3 +1,24 @@
+import { Column, JoinTable, ManyToMany } from 'typeorm';
+
+import { Wish } from '@/modules/wishes';
+
 import { BaseEntity } from '@/common/entities/baseEntity.entity';
 
-export class Wishlist extends BaseEntity {}
+export class Wishlist extends BaseEntity {
+  @Column({
+    length: 250,
+  })
+  name: string;
+
+  @Column({
+    length: 1500,
+  })
+  description: string;
+
+  @Column()
+  image: string;
+
+  @ManyToMany(() => Wish, (wish) => wish.id)
+  @JoinTable()
+  items: Wish[];
+}
