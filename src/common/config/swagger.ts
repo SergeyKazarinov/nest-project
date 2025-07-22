082102d385e8c05e8222c535dd7bc578ce79b0d8
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-22 23:35:49 +0500

    feat(swagger): addGlobalResponses

--- src/common/config/swagger.ts
+++ src/common/config/swagger.ts
@@ -1,9 +1,26 @@
 import { DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 
+import { ErrorMessageDto, ServerErrorMessageDto } from '../dto/error.dto';
+
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('NestJS API')
   .setDescription('NestJS API description')
   .setVersion('1.0')
+  .addGlobalResponse({
+    status: 500,
+    description: 'Внутренняя ошибка сервера',
+    type: ServerErrorMessageDto,
+  })
+  .addGlobalResponse({
+    status: 404,
+    description: 'Ресурс не найден',
+    type: ErrorMessageDto,
+  })
+  .addGlobalResponse({
+    status: 408,
+    description: 'Время ожидания запроса истекло',
+    type: ErrorMessageDto,
+  })
   .build();
 
 export const swaggerOptions: SwaggerCustomOptions = {};
