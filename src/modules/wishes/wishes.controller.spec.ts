9f2bc5a656037c088e957349f203e76b91a0fa2f
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-23 15:01:41 +0500

    test(wish): add base setting for a wish module

--- src/modules/wishes/wishes.controller.spec.ts
+++ src/modules/wishes/wishes.controller.spec.ts
@@ -6,16 +6,41 @@ import { WishesService } from './wishes.service';
 describe('WishesController', () => {
   let controller: WishesController;
 
+  const mockRepository = {
+    create: jest.fn(() => ({})),
+    findLast: jest.fn(),
+    findTop: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+    copy: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WishesController],
       providers: [WishesService],
-    }).compile();
+    })
+      .overrideProvider(WishesService)
+      .useValue(mockRepository)
+      .compile();
 
     controller = module.get<WishesController>(WishesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('create a Wish', () => {
+    const createWishDtoMock = {
+      name: 'Test Wish',
+      description: 'Test Description',
+      link: 'https://example.com',
+      image: 'https://example.com/image.jpg',
+      price: 100.5,
+    };
+
+    expect(controller.create(createWishDtoMock)).toEqual({});
   });
 });
--- src/modules/wishes/wishes.service.spec.ts
+++ src/modules/wishes/wishes.service.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+import { Wish } from './entities/wish.entity';
 import { WishesService } from './wishes.service';
 
 describe('WishesService', () => {
   let service: WishesService;
 
+  const mockRepository = {
+    create: jest.fn(),
+    findLast: jest.fn(),
+    findTop: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+    copy: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WishesService],
+      providers: [
+        WishesService,
+        {
+          provide: getRepositoryToken(Wish),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<WishesService>(WishesService);
