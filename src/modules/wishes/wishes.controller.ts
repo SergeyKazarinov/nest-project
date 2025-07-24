91d8c62ec7c3ea286e0f44cc2dd33ff2bf3aaad6
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-24 19:02:37 +0500

    feat(wish): add guard for wishes

--- src/modules/wishes/wishes.controller.ts
+++ src/modules/wishes/wishes.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 import {
   ApiCreateOperation,
@@ -17,42 +18,49 @@ export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
   @Post()
+  @ApiBearerAuth()
   @ApiCreateOperation('Создание подарка', Wish)
   create(@Body() createWishDto: CreateWishDto) {
     return this.wishesService.create(createWishDto);
   }
 
   @Get('/last')
+  @ApiBearerAuth()
   @ApiFindOperation('Получение списка последних подарков', Wish)
   findLast() {
     return this.wishesService.findLast();
   }
 
   @Get('/top')
+  @ApiBearerAuth()
   @ApiFindOperation('Получение списка популярных подарков', Wish)
   findTop() {
     return this.wishesService.findTop();
   }
 
   @Get(':id')
+  @ApiBearerAuth()
   @ApiFindOperation('Получение подарка по id', Wish)
   findOne(@Param('id') id: string) {
     return this.wishesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
   @ApiUpdateOperation('Обновление подарка', Wish)
   update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
     return this.wishesService.update(+id, updateWishDto);
   }
 
   @Delete(':id')
+  @ApiBearerAuth()
   @ApiDeleteOperation('Удаление подарка')
   remove(@Param('id') id: string) {
     return this.wishesService.remove(+id);
   }
 
   @Post(':id/copy')
+  @ApiBearerAuth()
   @ApiCreateOperation('Копирование подарка', Wish)
   copy(@Param('id') id: string) {
     return this.wishesService.copy(+id);
--- src/modules/wishes/wishes.module.ts
+++ src/modules/wishes/wishes.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { JwtAuthGuard } from '@/modules/auth/guard/jwt-guard';
 
 import { Wish } from './entities/wish.entity';
 import { WishesController } from './wishes.controller';
@@ -8,6 +11,12 @@ import { WishesService } from './wishes.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Wish])],
   controllers: [WishesController],
-  providers: [WishesService],
+  providers: [
+    WishesService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class WishesModule {}
