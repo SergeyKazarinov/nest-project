5824216931569b12025d8903720300c640e805c9
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-24 19:09:35 +0500

    feat(offer): add JwtAuthGuard for offer module

--- src/modules/offers/offers.module.ts
+++ src/modules/offers/offers.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { JwtAuthGuard } from '../auth/guard/jwt-guard';
 
 import { Offer } from './entities/offer.entity';
 import { OffersController } from './offers.controller';
@@ -8,6 +11,12 @@ import { OffersService } from './offers.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Offer])],
   controllers: [OffersController],
-  providers: [OffersService],
+  providers: [
+    OffersService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class OffersModule {}
