0eecf4f8741d22f1ec16a3be168d10065683f052
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-25 13:34:55 +0500

    fix(user): fix register deps for find user by username

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -41,7 +41,7 @@ export class UsersService {
 
   async findByUsername(username: string, options?: FindOneOptions<User>) {
     const user = await this.UsersRepository.findOne({
-      where: { username },
+      where: { username: ILike(username) },
       relations: ['wishes', 'offers', 'wishlists'],
       ...options,
     });
