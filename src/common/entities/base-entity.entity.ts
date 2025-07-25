5ea425b479b50932b0af0602c5f999998ab95a49
Sergey Kazarinov <kazarinov092@gmail.com>
2025-07-25 15:41:39 +0500

    docs(swagger): add api properties for a baseEntity

--- src/common/entities/base-entity.entity.ts
+++ src/common/entities/base-entity.entity.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate } from 'class-validator';
 import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
+  @ApiProperty({
+    description: 'ID сущности',
+    type: Number,
+    example: 0,
+  })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'Дата создания сущности',
+    type: Date,
+    example: new Date(),
+  })
   @CreateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
@@ -12,6 +23,11 @@ export abstract class BaseEntity {
   @IsDate()
   createdAt: Date;
 
+  @ApiProperty({
+    description: 'Дата обновления сущности',
+    type: Date,
+    example: new Date(),
+  })
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
